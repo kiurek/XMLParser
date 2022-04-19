090a96bd9d12f5a071811c206295f55479b87df1
kiurek <79983272+kiurek@users.noreply.github.com>
2022-04-19 21:28:18 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace XMLParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XMLParser.ZlecenieXDocumentParse();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ XMLParser.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace XMLParser
+{
+    class XMLParser
+    {
+        public static void ZlecenieXDocumentParse()
+        {
+            XDocument xml = XDocument.Load(@"C:\Users\kkure\source\repos\XMLParser\Zlecenie.xml");
+
+            IEnumerable<XElement> xElements = xml.Descendants("product_des");
+
+            IEnumerable<XElement> xElements1 = xml.Descendants("glasswidth");
+
+            IEnumerable<XElement> xElements2 = xml.Descendants("glassheight");
+
+            IEnumerable<XElement> xElements3 = xml.Descendants("pane_structure").Descendants("pane");
+
+            foreach (XElement element in xElements)
+            {
+                Console.WriteLine("Nazwa: " + element.Value);
+            }
+
+            foreach (XElement element in xElements1)
+            {
+                Console.WriteLine("Szerokość: " + element.Value);
+            }
+
+            foreach (XElement element in xElements2)
+            {
+                Console.WriteLine("Wysokość: " + element.Value);
+            }
+
+            foreach (XElement element in xElements3)
+            {
+                Console.WriteLine("Kolor: " + element.Value);
+            }
+        }
+    }
+}
